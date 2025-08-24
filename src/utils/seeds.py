7634221db040baf45d1c313befdfd44ebf546aef
delillo-florencia <delillo.florencia@gmail.com>
2025-08-24 23:17:32 +0200

    seeds code

---
+++ src/utils/seeds.py
@@ -0,0 +1,14 @@
+import torch
+import random
+import numpy as np
+import os
+
+def set_seed(seed=42):
+    """Set seed for reproducibility"""
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
