91d8f973fe9c391f021ad70099eda1bd3c68f285
delillo-florencia <delillo.florencia@gmail.com>
2025-08-26 17:07:27 +0200

    added inits and setup for package

---
+++ src/setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="VAE_multiomics",
+    version="0.1.0",
+    description="Tools for preprocessing and training multimodal autoencoders on gene and isoform expression data")
